9837661acbbb31f14a7b640df631cc3911483fae
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-08-29 09:29:13 -0400

    passed

--- src/range_of_list.py
+++ src/range_of_list.py
@@ -1,6 +1,6 @@
 # Write your solution here
 def range_of_list(list : list):
-    pass    
+    return max(list) - min(list)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
